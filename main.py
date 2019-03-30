73bc0eeb672606c9e04df31fa3e9579df50eba75
Ellen Hoeven <>
2019-03-30 21:10:37 +0100

    refactor: Include try except if no papers in /papers folder

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import load_pdfs
 
 if __name__ == '__main__':
     foldername, filenames = load_pdfs.getFileNames()
-    print(read_pdf.extract_text_from_pdf(foldername+'/'+filenames[0]))
+    try:
+    	print(read_pdf.extract_text_from_pdf(foldername+'/'+filenames[0]))
+    except:
+    	print("No papers in /papers yet")
