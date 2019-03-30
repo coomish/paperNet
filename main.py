f79f52efd69be002ccbd12510fd7bf3e2a4fdd2c
robin <robinhirt@gmail.com>
2019-03-29 23:40:22 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import read_pdf
+import load_pdfs
+
+if __name__ == '__main__':
+    foldername, filenames = load_pdfs.getFileNames()
+    print(read_pdf.extract_text_from_pdf(foldername+'/'+filenames[104]))
